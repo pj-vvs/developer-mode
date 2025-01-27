c145bf2cbfd57d120c2cb2bf9018bb3583640258
Prasad V <prasadraju.veepuri@gmail.com>
2025-01-28 00:30:14 +0530

    initial commit

---
+++ src/test/java/com/usecase/limitless/LimitlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usecase.limitless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
